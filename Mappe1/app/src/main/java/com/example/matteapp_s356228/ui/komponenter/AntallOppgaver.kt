e4f49555753ee6bbbaa69cd8611a824dad9676f6
magnusK <111736604+magnuskvandal@users.noreply.github.com>
2025-09-19 14:01:41 +0200

    laget komponenten AntallOppgaver og designet preferansesiden

---
+++ Mappe1/app/src/main/java/com/example/matteapp_s356228/ui/komponenter/AntallOppgaver.kt
@@ -0,0 +1,94 @@
+package com.example.matteapp_s356228.ui.komponenter
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.matteapp_s356228.R
+
+@Composable
+fun AntallOppgaver(
+    modifier: Modifier = Modifier,
+    valgtAntall: Int,
+    tilgjengeligeAntall: List<Int>,
+    onAntallValgt: (Int) -> Unit
+    ){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(
+            text = stringResource(R.string.antallOppgaver),
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .align(Alignment.Start)
+        )
+        Column(
+            modifier = Modifier.selectableGroup()
+        ){
+            tilgjengeligeAntall.forEach{ antall ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp)
+                        .selectable(
+                            selected = (antall == valgtAntall),
+                            onClick = { onAntallValgt(antall) },
+                            role = Role.RadioButton
+                        ),
+                    verticalAlignment = Alignment.CenterVertically,
+                ){
+                    RadioButton(
+                        selected = (antall == valgtAntall),
+                        onClick = { onAntallValgt(antall) },
+                        modifier = Modifier
+                            .size(size = 50.dp)
+                            .scale(scale = 1.3f)
+                    )
+                    Spacer(modifier = Modifier.width(width = 8.dp))
+                    Text(
+                        text = antall.toString(),
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+@Preview(showBackground = true, heightDp = 450, widthDp = 400)
+@Composable
+fun AntallOppgaverPreview() {
+    AntallOppgaver(
+        valgtAntall = 10,
+        tilgjengeligeAntall = listOf(5, 10, 20, 30, 40, 50),
+        onAntallValgt = {}
+    )
+}
--- Mappe1/app/src/main/java/com/example/matteapp_s356228/ui/sider/Preferanserside.kt
+++ Mappe1/app/src/main/java/com/example/matteapp_s356228/ui/sider/Preferanserside.kt
@@ -1,24 +1,53 @@
 package com.example.matteapp_s356228.ui.sider
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.matteapp_s356228.ui.komponenter.TopBar
 import com.example.matteapp_s356228.ui.theme.Matteapp_s356228Theme
 import com.example.matteapp_s356228.R
+import com.example.matteapp_s356228.ui.komponenter.AntallOppgaver
 
 @Composable
-fun Prefereanserside(
+fun Preferanserside(
     modifier: Modifier = Modifier
 ){
+    // Midlertidig state management for valgt antall oppgaver
+    var valgtAntallState by remember { mutableStateOf(5) }
+    val muligeAntall = listOf(5, 10, 15)
+
     Scaffold(
         modifier = modifier,
         topBar = { TopBar(tittel = stringResource(R.string.preferanser), onNavigateBack = { /*TODO*/ }) }
     ){ innerPadding ->
-        Text(text ="midlertidig tekst" , modifier = Modifier.padding(innerPadding))
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(all = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            AntallOppgaver(
+                valgtAntall = valgtAntallState,
+                tilgjengeligeAntall = muligeAntall,
+                onAntallValgt = {valgtAntall -> valgtAntallState = valgtAntall},
+                modifier = Modifier
+            )
+        }
     }
 }
 
@@ -28,6 +57,6 @@ fun Prefereanserside(
 @Composable
 fun PreferansersidePreview() {
     Matteapp_s356228Theme(dynamicColor = false) {
-        Prefereanserside()
+        Preferanserside()
     }
 }
