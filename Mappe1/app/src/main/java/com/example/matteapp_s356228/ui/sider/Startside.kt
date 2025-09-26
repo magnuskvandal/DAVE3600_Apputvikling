870b486fe7024215f135280340ab1801b88b9713
magnusK <111736604+magnuskvandal@users.noreply.github.com>
2025-09-26 17:58:28 +0200

    lagt til app-ikonet på startsiden

--- Mappe1/app/src/main/java/com/example/matteapp_s356228/ui/sider/Startside.kt
+++ Mappe1/app/src/main/java/com/example/matteapp_s356228/ui/sider/Startside.kt
@@ -1,11 +1,14 @@
 package com.example.matteapp_s356228.ui.sider
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Calculate
@@ -16,6 +19,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -40,7 +45,14 @@ fun Startside(
             modifier = Modifier.fillMaxWidth().padding(all = 15.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ){
-            Spacer (modifier = Modifier.height(100.dp))
+            Spacer (modifier = Modifier.height(80.dp))
+            Image(
+                painter = painterResource(id = R.drawable.app_ikon),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+            )
+            Spacer (modifier = Modifier.height(10.dp))
             Text(
                 text = stringResource(R.string.tittel),
                 style = MaterialTheme.typography.displayMedium,
@@ -49,7 +61,7 @@ fun Startside(
                 color = MaterialTheme.colorScheme.onBackground
             )
 
-            Spacer(modifier = Modifier.height(60.dp))
+            Spacer(modifier = Modifier.height(50.dp))
 
             Navigasjonskort(
                 tekst = stringResource(R.string.startSpill),
