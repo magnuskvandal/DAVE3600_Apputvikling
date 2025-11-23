b213cd8568d28da47d2368a2c34cf2a0bebc1377
magnusK <111736604+magnuskvandal@users.noreply.github.com>
2025-11-23 22:20:37 +0100

    Fjernet Preview fra DeleteConfirmDialog.kt

--- Mappe3/app/src/main/java/com/example/mittKart_s356228/ui/components/DeleteConfirmationDialog.kt
+++ Mappe3/app/src/main/java/com/example/mittKart_s356228/ui/components/DeleteConfirmationDialog.kt
@@ -11,10 +11,8 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mittKart_s356228.R
-import com.example.mittKart_s356228.ui.theme.MittKart_s356228Theme
 
 @Composable
 fun DeleteConfirmationDialog(
@@ -62,17 +60,4 @@ fun DeleteConfirmationDialog(
             }
         }
     )
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun DeleteConfirmationDialogPreview(){
-    MittKart_s356228Theme {
-        DeleteConfirmationDialog(
-            placeName = "Test Place",
-            onConfirm = {},
-            onDismiss = {}
-        )
-    }
 }
